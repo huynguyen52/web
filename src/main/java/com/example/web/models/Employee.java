92ce40626fc90ad72c806d7a4210f07fcaea03bd
Linh Nguyen <lnguyen>
2023-03-07 16:25:32 +0700

    Hibernate Composite Primary Key using @Embedded

--- src/main/java/com/example/web/models/Employee.java
+++ src/main/java/com/example/web/models/Employee.java
@@ -4,22 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Table(name = "employee_tbl")
-@IdClass(EmployeePK.class)
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
-    @Id
-    private int employeeId;
-    @Id
-    private int deptId;
+    @EmbeddedId
+    private EmployeePK employeePKId;
     private String name;
     private String email;
     private String phone;
--- src/main/java/com/example/web/models/EmployeePK.java
+++ src/main/java/com/example/web/models/EmployeePK.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Embeddable;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import java.io.Serializable;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class EmployeePK implements Serializable {
     private int employeeId;
     private int deptId;
